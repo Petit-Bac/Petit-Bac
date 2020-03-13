059e576a161014f891c6b37eb722389276f74beb
ClubTic0 <62136504+ClubTic0@users.noreply.github.com>
2020-03-13 11:07:21 +0100

    Add files via upload

---
+++ projet.py
@@ -0,0 +1,18 @@
+﻿alphabet=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
+import random
+lettre = random.choice(alphabet)
+
+#print(lettre)
+
+score = 0
+
+reponse = input('la lettre est '+lettre)
+reponse = reponse.capitalize()
+
+if reponse[0] == lettre:
+    print("c'est bon")
+    score =+ 1
+else:
+    print("c'est faux")
+print(score)
